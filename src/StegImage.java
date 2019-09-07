84a3cca007ddb27abd87be9a4a919ffc79b5e299
SterL <sterlingzerr@gmail.com>
2019-09-07 01:47:42 -0600

    refactoring

--- src/StegImage.java
+++ src/StegImage.java
@@ -2,10 +2,10 @@
  * Written by Sterling Zerr 9/20/16
  *
  * This class handles reading and writing of image files. You can pass the path
- * of an image and the path of the output image to the constructor.
- * This will allow you to retrieve the byte array of the image file. You can
- * then alter the images RGB values from the byte array, and then save the newly
- * altered pixels into a new image.
+ * of an image and the path of the output image to the constructor. This will
+ * allow you to retrieve the byte array of the image file. You can then alter
+ * the images RGB values from the byte array, and then save the newly altered
+ * pixels into a new image.
  */
 
 import java.io.File;
@@ -17,113 +17,114 @@ import javax.imageio.ImageIO;
 
 class StegImage {
 
-    private BufferedImage inputImage;
-    private BufferedImage outputImage;
-    private File inputFile;
-    private File outputFile;
-    private String inputFilePath;
-    private String outputFilePath;
-    private String fileType;
-    private int height;
-    private int width;
-    private int[] pixels;
+  private BufferedImage inputImage;
+  private BufferedImage outputImage;
+  private File inputFile;
+  private File outputFile;
+  private String inputFilePath;
+  private String outputFilePath;
+  private String fileType;
+  private int height;
+  private int width;
+  private int[] pixels;
 
-    /**
-     * Constructor that takes a path to an input image, and a path to an
-     * output image. This saves the concealed image to a new, near-duplicate
-     * image instead of overwriting the original.
-     *
-     * @param path
-     * @param inputFileName
-     * @param outputFileName
-     */
-    public StegImage(String inputFilePath, String outputFilePath) {
-        try {
-            this.inputFilePath = extractFileName(inputFilePath);
-            this.outputFilePath = extractFileName(outputFilePath);
-            this.fileType = inputFilePath.substring(inputFilePath.length() - 4, inputFilePath.length());
-            this.inputFile = new File(inputFilePath);
-            this.outputFile = new File(outputFilePath);
-            this.inputImage = ImageIO.read(inputFile);
-            this.height = inputImage.getHeight();
-            this.width = inputImage.getWidth();
-            this.outputImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB); // FOR BMP FILES
-            this.pixels = getByteArray();
+  /**
+   * Constructor that takes a path to an input image, and a path to an output
+   * image. This saves the concealed image to a new, near-duplicate image
+   * instead of overwriting the original.
+   *
+   * @param path
+   * @param inputFileName
+   * @param outputFileName
+   */
+  public StegImage(String inputFilePath, String outputFilePath) {
+    try {
+      this.inputFilePath = extractFileName(inputFilePath);
+      this.outputFilePath = extractFileName(outputFilePath);
+      this.fileType = inputFilePath.substring(inputFilePath.length() - 4, inputFilePath.length());
+      this.inputFile = new File(inputFilePath);
+      this.outputFile = new File(outputFilePath);
+      this.inputImage = ImageIO.read(inputFile);
+      this.height = inputImage.getHeight();
+      this.width = inputImage.getWidth();
+      this.outputImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB); // FOR BMP FILES
+      this.pixels = getByteArray();
 
-        } catch (IOException e) {
-            System.out.println("Error: " + e);
-        }
+    } catch (IOException e) {
+      System.out.println("Error: " + e);
     }
+  }
 
-    /**
-     * Constructor that takes a path to an input image.
-     * This saves the encoded image to a new, near-duplicate
-     * image instead of overwriting the original.
-     *
-     * @param inputFileName
-     */
-    public StegImage(String inputFilePath) {
-        try {
-            this.inputFilePath = extractFileName(inputFilePath);
-            this.fileType = inputFilePath.substring(inputFilePath.length() - 4, inputFilePath.length());
-            this.inputFile = new File(inputFilePath);
-            this.inputImage = ImageIO.read(inputFile);
-            this.height = inputImage.getHeight();
-            this.width = inputImage.getWidth();
-//            this.outputImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB); // FOR BMP FILES
-        } catch (IOException e) {
-            System.out.println("Error: " + e);
-        }
+  /**
+   * Constructor that takes a path to an input image. This saves the encoded
+   * image to a new, near-duplicate image instead of overwriting the original.
+   *
+   * @param inputFileName
+   */
+  public StegImage(String inputFilePath) {
+    try {
+      this.inputFilePath = extractFileName(inputFilePath);
+      this.fileType = inputFilePath.substring(inputFilePath.length() - 4, inputFilePath.length());
+      this.inputFile = new File(inputFilePath);
+      this.inputImage = ImageIO.read(inputFile);
+      this.height = inputImage.getHeight();
+      this.width = inputImage.getWidth();
+      //this.outputImage = new BufferedImage(width, height, BufferedImage
+      // .TYPE_INT_RGB); // FOR BMP FILES
+    } catch (IOException e) {
+      System.out.println("Error: " + e);
     }
+  }
 
-    /**
-     * This method returns the byte array in an int[] of the image. From this
-     * data, you can retrieve the RGB values of each pixel.
-     *
-     * @return
-     */
-    public int[] getByteArray() {
-        return inputImage.getRGB(0, 0, width, height, null, 0, width);
-    }
+  /**
+   * This method returns the byte array in an int[] of the image. From this
+   * data, you can retrieve the RGB values of each pixel.
+   *
+   * @return
+   */
+  public int[] getByteArray() {
+    return inputImage.getRGB(0, 0, width, height, null, 0, width);
+  }
 
-    /**
-     * This method saves a byte array (newPixels) to the image container
-     * specified as outputFileName. The image will be saved to the same dir as
-     * the input image.
-     *
-     * @param newPixels
-     * @return
-     */
-//    public boolean saveImage(int[] newPixels) {
-    public boolean saveImage() {
-        try {
-            outputImage.setRGB(0, 0, width, height, this.pixels, 0, width);
-            if (ImageIO.write(outputImage, "bmp", outputFile) == true) {
-                System.out.println("The Steganografied image has been saved successfully to: " + this.outputFile.getAbsolutePath());//outputFilePath);
-                return true;
-            } else {
-                System.out.println("Error: The file was not saved successfully");
-                return false;
-            }
-        } catch (IOException ex) {
-            Logger.getLogger(StegImage.class.getName()).log(Level.SEVERE, null, ex);
-            return false;
-        }
+  /**
+   * This method saves a byte array (newPixels) to the image container specified
+   * as outputFileName. The image will be saved to the same dir as the input
+   * image.
+   *
+   * @param newPixels
+   *
+   * @return
+   */
+  public boolean saveImage() {
+    try {
+      outputImage.setRGB(0, 0, width, height, this.pixels, 0, width);
+      if (ImageIO.write(outputImage, "bmp", outputFile) == true) {
+        System.out.println("The Steganografied image has been saved successfully to: " + this.outputFile.getAbsolutePath());//outputFilePath);
+        return true;
+      } else {
+        System.out.println("Error: The file was not saved successfully");
+        return false;
+      }
+    } catch (IOException ex) {
+      Logger.getLogger(StegImage.class.getName()).log(Level.SEVERE, null, ex);
+      return false;
     }
+  }
 
-    public void setPixels(int[] pixels) {
-        this.pixels = pixels;
-    }
+  public void setPixels(int[] pixels) {
+    this.pixels = pixels;
+  }
 
-    /**
-     * This method returns the filename from the provided path
-     *
-     * @param path
-     * @return
-     */
-    private String extractFileName(String path) {
-        String[] strings = path.split("\\\\");
-        return strings[strings.length - 1];
-    }
+  /**
+   * This method returns the filename from the provided path
+   *
+   * @param path
+   *
+   * @return
+   */
+  private String extractFileName(String path) {
+    String[] strings = path.split("\\\\");
+    return strings[strings.length - 1];
+  }
 
 }
--- src/Steganography.java
+++ src/Steganography.java
@@ -1,9 +1,12 @@
 /**
- * Written by Sterling Zerr 9/20/16
- * This is the main class which takes 3 or 5 arguments depending on the operation:
+ * This is the main class which takes 3 or 5 arguments depending on the
+ * operation:
+ *
  * 1.) Operation to perform:
- * a.) conceal - steganografies a message into the image
- * b.) reveal  - reveals the steganografied message in image
+ * a.) conceal - steganografies a message into the
+ * image
+ * b.) revseal  - reveals the steganografied message in image
+ *
  * 2.) Flags of which color bytes to conceal the message into (-rgb)
  * a.) '-rgb'
  * b.) '-rg'
@@ -12,79 +15,67 @@
  * e.) '-r'
  * f.) '-b'
  * g.) '-g'
+ *
  * 3.) Path of the input image to conceal the message into
  * 4.) Path of the output image which holds the concealed message
  * 5.) Message to conceal into the image
  */
-
 public class Steganography {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        String operation = null;
-        String flags = null;
-        String inputFilePath = null;
+    // if these conditions aren't met, show usage
+    if (args.length != 0 && args.length > 2 &&
+      (args[0].equals("conceal") || args[0].equals("reveal"))) {
+      // TODO - regex checking for valid flags
+      String flags = args[1];
+      String inputFilePath = args[2];
 
-        if (args.length > 0 && !args[0].equals("help")) {
-            if (args[0].equals("conceal") || args[0].equals("reveal")) {
-                operation = args[0];
-            } else {
-                System.out.println("Error: invalid operation - use 'conceal' or 'reveal'");
-                System.exit(0);
-            }
-        } else {
-            System.out.println("\nUsage: \n" +
-                    "    conceal <flags> <inputPath> <outputPath> <message>\n" +
-                    "    reveal <flags> <inputPath>\n\n" +
-                    "    operation: 'conceal' or 'reveal'\n" +
-                    "    flags: '-rgb' or '-rg' or '-rb' or '-bg' or '-r' or '-b' or '-g'\n" +
-                    "    inputPath: path of the input image to coneal the message into\n" +
-                    "    outputPath: path of the output image which holds the concealed message\n" +
-                    "    message: message to conceal into the image");
-            System.exit(0);
-        }
-
-        // TODO - regex checking for valid flags
-        flags = args[1];
-
-        if (args.length == 3) {
-            inputFilePath = args[2];
-        }
+      if (args[0].equals("conceal")) {
         if (args.length == 5) {
-            inputFilePath = args[2];
+          String outputFilePath = args[3];
+          String message = args[4];
+
+          StegImage si = new StegImage(inputFilePath, outputFilePath);
+          Steganografier Steganografier = new Steganografier(si);
+          Steganografier.selectableSteganographi(message, flags);
+          String decodedMessage = Steganografier.selectableDesteganographi(flags);
+
+          // before saving the image, this checks to make sure the
+          // encoded message is the same as the decoded message
+          if (message.equals(decodedMessage)) {
+            si.saveImage();
+          } else {
+            System.out.println("There was an error concealing the messge");
+            System.out.println(message + "   !=   " + decodedMessage);
+          }
+
+        } else {
+          System.out.println("Error: invalid number of arguments for conceal");
+          System.exit(0);
         }
-        if (args.length > 5) {
-            System.out.println("Error: invalid number of arguments");
-            System.exit(0);
+
+      } else if (args[0].equals("reveal")) {
+        if (args.length == 3) {
+          StegImage si = new StegImage(inputFilePath);
+          Steganografier Steganografier = new Steganografier(si);
+          String decodedMessage = Steganografier.selectableDesteganographi(flags);
+          System.out.println(decodedMessage);
+        } else {
+          System.out.println("Error: invalid number of arguments for reveal");
+          System.exit(0);
         }
+      }
 
-        if (operation.equals("conceal")) {
-            if (args.length > 4 && args[3] != null && args[4] != null) {
-                String outputFilePath = args[3];
-                String message = args[4];
-
-                StegImage si = new StegImage(inputFilePath, outputFilePath);
-                Steganografier Steganografier = new Steganografier(si);
-                Steganografier.selectableSteganographi(message, flags);
-                String decodedMessage = Steganografier.selectableDesteganographi(flags);
-
-                if (message.equals(decodedMessage)) {
-                    si.saveImage();
-                } else {
-                    System.out.println("There was an error concealing the messge");
-                    System.out.println(message + "   !=   " + decodedMessage);
-                }
-
-            } else {
-                System.out.println("Error: invalid number of arguments");
-                System.exit(0);
-            }
-
-        } else if (operation.equals("reveal")) {
-            StegImage si = new StegImage(inputFilePath);
-            Steganografier Steganografier = new Steganografier(si);
-            String decodedMessage = Steganografier.selectableDesteganographi(flags);
-            System.out.println(decodedMessage);
-        }
+    } else {
+      System.out.println("\nUsage: \n" +
+        "    conceal <flags> <inputPath> <outputPath> <message>\n" +
+        "    reveal <flags> <inputPath>\n\n" +
+        "    flags: '-rgb' or '-rg' or '-rb' or '-bg' or '-r' or '-b' or '-g'\n" +
+        "    inputPath: path of the input image to coneal the message into\n" +
+        "    outputPath: path of the output image which holds the concealed message\n" +
+        "    message: \"message\" to conceal into the image");
+      System.exit(0);
     }
+  }
 }
